a76aa6107ba4419bc94cda213ccd13b4d90076d4
shaunhurryup <1600083715@qq.com>
2024-08-06 00:57:44 +0800

    feat: 自定义的原始 html

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,9 +22,10 @@ repositories {
 }
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
-dependencies {
-//    implementation(libs.exampleLibrary)
-}
+//dependencies {
+//    implementation("org.jetbrains:marketplace-zip-signer:0.1.6")
+//    implementation("com.jetbrains:plugins:1.1.1")
+//}
 
 // Set the JVM language level used to build the project.
 kotlin {
--- src/main/kotlin/com/github/shaunhurryup/easycoderjetbrainsplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/shaunhurryup/easycoderjetbrainsplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,7 +1,5 @@
 package com.github.shaunhurryup.easycoderjetbrainsplugin.toolWindow
 
-import com.github.shaunhurryup.easycoderjetbrainsplugin.MyBundle
-import com.github.shaunhurryup.easycoderjetbrainsplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -10,8 +8,13 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
+import com.github.shaunhurryup.easycoderjetbrainsplugin.MyBundle
+import com.github.shaunhurryup.easycoderjetbrainsplugin.services.MyProjectService
+import com.intellij.ui.jcef.JBCefApp
+import com.intellij.ui.jcef.JBCefBrowser
+import java.awt.BorderLayout
 import javax.swing.JButton
-
+import javax.swing.JPanel
 
 class MyToolWindowFactory : ToolWindowFactory {
 
@@ -32,14 +35,30 @@ class MyToolWindowFactory : ToolWindowFactory {
         private val service = toolWindow.project.service<MyProjectService>()
 
         fun getContent() = JBPanel<JBPanel<*>>().apply {
-            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            layout = BorderLayout()
 
-            add(label)
-            add(JButton(MyBundle.message("shuffle")).apply {
+            val label = JBLabel(MyBundle.message("randomLabel", "?"))
+            val topPanel = JPanel()
+            topPanel.add(label)
+            topPanel.add(JButton(MyBundle.message("shuffle")).apply {
                 addActionListener {
                     label.text = MyBundle.message("randomLabel", service.getRandomNumber())
                 }
             })
+            add(topPanel, BorderLayout.NORTH)
+
+            // 使用 JBCefBrowser 加载本地 HTML 文件
+            if (!JBCefApp.isSupported()) {
+                throw RuntimeException("JCEF is not supported")
+            }
+
+            // 创建 JBCefClient 实例
+            val client = JBCefApp.getInstance().createClient()
+
+            // 创建 JBCefBrowser 实例
+            val browser = JBCefBrowser("file://path/to/dist/index.html")
+
+            add(browser.component, BorderLayout.CENTER)
         }
     }
 }
