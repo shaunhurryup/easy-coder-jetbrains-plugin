dd2607d568dbaeeb884c91d2ce202d9bf4864ab0
shaunhurryup <1600083715@qq.com>
2024-08-21 23:13:02 +0800

    feat: 右键菜单的代码解释

--- src/main/java/com/easycoder/intellij/actions/assistants/ExplainCode.java
+++ src/main/java/com/easycoder/intellij/actions/assistants/ExplainCode.java
@@ -1,13 +1,13 @@
 package com.easycoder.intellij.actions.assistants;
 
-import com.easycoder.intellij.constant.PrefixString;
+import com.easycoder.intellij.enums.MessageId;
+import com.easycoder.intellij.model.WebviewMessage;
 import com.easycoder.intellij.services.EasyCoderSideWindowService;
-import com.easycoder.intellij.utils.EditorUtils;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.codeInspection.util.IntentionName;
-import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -17,8 +17,6 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NotNull;
@@ -61,42 +59,25 @@ public class ExplainCode extends DumbAwareAction implements IntentionAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        System.out.println(PropertiesComponent.getInstance().getValue("easycoder:token"));
 
         Project project = e.getData(LangDataKeys.PROJECT);
-//        if (Objects.isNull(project)) {
-//            return;
-//        }
         ApplicationManager.getApplication().invokeLater(() -> {
-//            VirtualFile vf = e.getRequiredData(CommonDataKeys.VIRTUAL_FILE);
-//            Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
-//            if (EditorUtils.isNoneTextSelected(editor)) {
-//                return;
-//            }
-//            PsiFile psiFile = e.getRequiredData(CommonDataKeys.PSI_FILE);
-//            JsonObject jsonObject = EditorUtils.getFileSelectionDetails(editor, psiFile, true, PrefixString.EXPLAIN_CODE);
-//
-//            // 手动处理需要转义的字段
-//            String selectedText = jsonObject.get("sendText").getAsString();
-//            selectedText = selectedText
-//                    .replace("\\", "\\\\")   // 转义反斜杠
-//                    .replace("\"", "\\\"")   // 转义双引号
-//                    .replace("\n", "\\n")    // 转义换行符
-//                    .replace("\r", "\\r");   // 转义回车符
-//
-//            jsonObject.addProperty("selectedText", selectedText);
+            Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+            String selectedText = editor.getSelectionModel().getSelectedText();
+            if (Objects.isNull(selectedText)) {
+                return;
+            }
 
-            JsonObject result = new JsonObject();
-//            ToolWindowManager tool = ToolWindowManager.getInstance(project);
-//            Objects.requireNonNull(tool.getToolWindow("EasyCoder")).activate(() -> {
-//                if (logger.isDebugEnabled()) {
-//                    logger.debug("******************* ExplainCode Enabled EasyCoder window *******************");
-//                }
-//            }, true, true);
-//            jsonObject.addProperty("fileName", vf.getName());
-//            jsonObject.addProperty("filePath", vf.getCanonicalPath());
-            result.addProperty("id", "test");
-            project.getService(EasyCoderSideWindowService.class).notifyIdeAppInstance(result);
+            JsonObject payload = new JsonObject();
+            payload.addProperty("content", selectedText);
+            payload.addProperty("command", "帮我解释这段代码");
+
+            WebviewMessage request = WebviewMessage.builder()
+                    .id(MessageId.ExplainCode_Menu)
+                    .payload(payload)
+                    .build();
+
+            project.getService(EasyCoderSideWindowService.class).notifyIdeAppInstance(new Gson().toJson(request));
         }, ModalityState.NON_MODAL);
     }
 }
