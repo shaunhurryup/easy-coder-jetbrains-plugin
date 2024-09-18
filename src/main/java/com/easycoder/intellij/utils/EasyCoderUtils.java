780cad2df819a5f90f12daa28569b56d67c9414f
shaunhurryup <1600083715@qq.com>
2024-09-18 20:50:40 +0800

    chore: clean code

--- src/main/java/com/easycoder/intellij/utils/EasyCoderUtils.java
+++ src/main/java/com/easycoder/intellij/utils/EasyCoderUtils.java
@@ -1,25 +1,15 @@
 package com.easycoder.intellij.utils;
 
-import com.alibaba.fastjson2.JSONObject;
-import com.easycoder.intellij.constant.PrefixString;
-import com.easycoder.intellij.model.GenerateModel;
-import com.easycoder.intellij.settings.EasyCoderSettings;
+import java.util.Objects;
+
 import com.easycoder.intellij.widget.EasyCoderWidget;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.editor.InlayModel;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class EasyCoderUtils {
     public static String includePreText(String preText, String language, String text) {
@@ -41,63 +31,6 @@ public class EasyCoderUtils {
         } else {
             return end;
         }
-    }
-
-    public static JsonObject pakgHttpRequestBodyForCPU(EasyCoderSettings settings, String prefix, String suffix) {
-        JsonObject httpBody = new JsonObject();
-        httpBody.addProperty("input_prefix", prefix);
-        httpBody.addProperty("input_suffix", suffix);
-        httpBody.addProperty("n_predict", Integer.parseInt(settings.getCompletionMaxToken().getDescription()));
-        httpBody.addProperty("temperature", 0.2);
-        httpBody.addProperty("repetition_penalty", 1.0);
-        httpBody.addProperty("top_k", 10);
-        httpBody.addProperty("top_p", 0.95);
-        // httpBody.addProperty("prompt", PrefixString.REQUST_END_TAG +
-        // easyCoderPrompt);
-        // httpBody.addProperty("frequency_penalty", 1.2);
-        // httpBody.addProperty("n_predict",
-        // Integer.parseInt(settings.getCompletionMaxToken().getDescription()));
-        // httpBody.addProperty("stream", false);
-        // JsonArray stopArray = new JsonArray();
-        // stopArray.add("|<end>|");
-        // httpBody.add("stop", stopArray);
-        return httpBody;
-    }
-
-    public static JsonObject pakgHttpRequestBodyForGPU(EasyCoderSettings settings, String easyCoderPrompt) {
-        JsonObject httpBody = new JsonObject();
-        httpBody.addProperty("inputs", easyCoderPrompt);
-        JsonObject parameters = new JsonObject();
-        parameters.addProperty("max_new_tokens", Integer.parseInt(settings.getCompletionMaxToken().getDescription()));
-        httpBody.add("parameters", parameters);
-        return httpBody;
-    }
-
-    public static String parseHttpResponseContentForCPU(EasyCoderSettings settings, String responseBody,
-            Pattern pattern) {
-        String generatedText = "";
-        Matcher matcher = pattern.matcher(responseBody);
-        StringBuilder contentBuilder = new StringBuilder();
-        while (matcher.find()) {
-            String jsonString = matcher.group(1);
-            JSONObject json = JSONObject.parseObject(jsonString);
-            String content = json.getString("content");
-            if (StringUtils.equalsAny(content, "<|endoftext|>", "")) {
-                continue;
-            }
-            contentBuilder.append(content);
-        }
-        return contentBuilder.toString().replace(PrefixString.RESPONSE_END_TAG, "");
-    }
-
-    public static String parseHttpResponseContentForGPU(EasyCoderSettings settings, String responseBody) {
-        String generatedText = "";
-        Gson gson = new Gson();
-        GenerateModel generateModel = gson.fromJson(responseBody, GenerateModel.class);
-        if (StringUtils.isNotBlank(generateModel.getGenerated_text())) {
-            generatedText = generateModel.getGenerated_text();
-        }
-        return generatedText.replace(PrefixString.RESPONSE_END_TAG, "");
     }
 
     public static String getIDEVersion(String whichVersion) {
